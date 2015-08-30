24da4352d4e0ad1093179db1b7823b571857cf60
Svendawg <Svendawg@users.noreply.github.com>
2015-08-30 12:52:53 -0400

    Update ModelStatsThesis28.8.14.R

--- ModelStatsThesis28.8.14.R
+++ ModelStatsThesis28.8.14.R
@@ -1,18 +1,23 @@
 ###Program for performing regression analysis of my thesis data, 
 ###written by Steve Scholle, Chris Castorena and Stacy Scholle 
-###Fall 2012 and Spring 2013###
+###Spring and Summer 2015; last modified 27 June 2015
 require(glmnet);
 require(parcor);
 require(lars);
 set.seed(1);
-source('getModels.r');
-source('getData.r');
+source('getModels.R');
+source('getData.R');
+source('getPlots.R');
 data <- getData('ThesisData.csv', 'irradiance.RData');
 
-## sample 75% of data for testing and 25% for testing
+## sample 75% of data for training and 25% for testing
 trainingDataIndices <- sample(1:nrow(data), nrow(data)*0.75, replace=FALSE);
-trainingData <- data[trainingDataIndices,];
-testData <- data[-trainingDataIndices,];
+trainingData <- data[trainingDataIndices, ];
+testData <- data[-trainingDataIndices, ];
 
+## make models
 model.stepwise <- getStepwiseModel(trainingData);
 model.Lasso <- getLassoModel(trainingData);
+
+## make plots
+AIC_valid_plot <- mk_AIC_valid();
